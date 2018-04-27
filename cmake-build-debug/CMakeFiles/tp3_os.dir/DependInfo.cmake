0f2ff765bb1ab463cfa1fc5dce7f3fe3bc3347b9
Eduard Voiculescu <eduardvoiculescu95@gmail.com>
2018-04-27 15:32:47 -0400

    CmakeList

---
+++ cmake-build-debug/CMakeFiles/tp3_os.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/eduardvoiculescu/Documents/tp3_os/src/common.c" "/Users/eduardvoiculescu/Documents/tp3_os/cmake-build-debug/CMakeFiles/tp3_os.dir/src/common.c.o"
+  "/Users/eduardvoiculescu/Documents/tp3_os/src/pm.c" "/Users/eduardvoiculescu/Documents/tp3_os/cmake-build-debug/CMakeFiles/tp3_os.dir/src/pm.c.o"
+  "/Users/eduardvoiculescu/Documents/tp3_os/src/pt.c" "/Users/eduardvoiculescu/Documents/tp3_os/cmake-build-debug/CMakeFiles/tp3_os.dir/src/pt.c.o"
+  "/Users/eduardvoiculescu/Documents/tp3_os/src/tlb.c" "/Users/eduardvoiculescu/Documents/tp3_os/cmake-build-debug/CMakeFiles/tp3_os.dir/src/tlb.c.o"
+  "/Users/eduardvoiculescu/Documents/tp3_os/src/vmm.c" "/Users/eduardvoiculescu/Documents/tp3_os/cmake-build-debug/CMakeFiles/tp3_os.dir/src/vmm.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/tp3_os.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tp3_os.dir/src/common.c.o"
+  "CMakeFiles/tp3_os.dir/src/pm.c.o"
+  "CMakeFiles/tp3_os.dir/src/pt.c.o"
+  "CMakeFiles/tp3_os.dir/src/tlb.c.o"
+  "CMakeFiles/tp3_os.dir/src/vmm.c.o"
+  "tp3_os.pdb"
+  "tp3_os"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tp3_os.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
